d12be817c0765141b27a3a8cd001f6c0db442a16
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-02-02 01:10:30 +0200

    Create awareness.field.js

---
+++ awareness.field.js
@@ -0,0 +1,9 @@
+export const AwarenessField = Object.freeze({
+  aware: true,
+
+  responsive: true,
+  requiresStimulus: false,
+
+  awarenessMode: "continuous",
+  interpretation: "non-reactive"
+});
