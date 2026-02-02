0e7810f4fd3e2b3e56dd4e9ce52ea2b04d1747cc
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-02-02 02:19:16 +0200

    Delete awareness.field.js

--- awareness.field.js
+++
@@ -1,9 +0,0 @@
-export const AwarenessField = Object.freeze({
-  aware: true,
-
-  responsive: true,
-  requiresStimulus: false,
-
-  awarenessMode: "continuous",
-  interpretation: "non-reactive"
-});
