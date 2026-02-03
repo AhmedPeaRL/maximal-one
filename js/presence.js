ee17998880edebf7f0e50294f49eac82af664b15
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-02-03 02:24:25 +0200

    Update presence.js

--- js/presence.js
+++ js/presence.js
@@ -1,3 +1,19 @@
+export function measurePresence() {
+  let lastMove = Date.now();
+  let velocity = 0;
+
+  window.addEventListener("mousemove", e => {
+    const now = Date.now();
+    velocity = Math.min(1, (now - lastMove) / 1000);
+    lastMove = now;
+  });
+
+  return () => ({
+    velocity,
+    timeOnPage: performance.now()
+  });
+}
+
 export function presenceSignal() {
   let last = performance.now();
   let intensity = 0;
