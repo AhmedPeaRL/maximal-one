5c980ec8924903a45a54e0ddf709114aae7621b6
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-02-06 22:21:32 +0200

    Create presence.js

---
+++ presence/presence/presence.js
@@ -0,0 +1,10 @@
+// This page does not listen.
+// This page does not respond.
+// This page does not record.
+
+// Time passes.
+// Presence remains.
+
+setInterval(() => {
+  // intentionally empty
+}, 1000);
