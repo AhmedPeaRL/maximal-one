948b325adf0d8bd7b38460fb813010fb294a4914
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-02-13 04:08:29 +0200

    Update sensitivity.js

--- core-scientific/validation/sensitivity.js
+++ core-scientific/validation/sensitivity.js
@@ -1,13 +1,9 @@
 import { computeTemporalPresence } from "../kernel/temporal-model.js";
 
 export function sensitivityScan(events) {
-  const lambdas = [0.0005, 0.001, 0.002, 0.005];
-  const results = [];
-
-  for (const lambda of lambdas) {
-    const value = computeTemporalPresence(events, lambda);
-    results.push({ lambda, value });
-  }
-
-  return results;
+  const lambdas = [0.0005, 0.001, 0.002, 0.005, 0.01];
+  return lambdas.map(lambda => ({
+    lambda,
+    value: computeTemporalPresence(events, lambda)
+  }));
 }
