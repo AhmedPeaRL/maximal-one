ebdf4b1817eec3a6921b1d90cbcea4727bbeeb13
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-02-25 08:43:08 +0200

    Update hash_gate.py

--- repro-core/hash_gate.py
+++ repro-core/hash_gate.py
@@ -13,7 +13,7 @@ def compute_hash():
     return result.decode().strip()
 
 def main():
-    expected = "3aa16caaee4dada25ee37b944e598d6e973bccfbdc5230c58f077d588bed9946"
+    expected = "dbef07e58445391ace4a9e60651e04f8f4aa90545aafad4f44814e3f4aeba0fb"
     current = compute_hash()
 
     if current != expected:
