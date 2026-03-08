a73db023ea5428b28fa0e1887df8481a3a08810d
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-03-08 04:46:53 +0200

    Create chaos_data_crawler.py

---
+++ analysis/chaos_data_crawler.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+from pathlib import Path
+
+
+DATASETS = {
+    "sunspots":
+    "https://raw.githubusercontent.com/jbrownlee/Datasets/master/monthly-sunspots.csv"
+}
+
+
+def download():
+
+    Path("real-data").mkdir(exist_ok=True)
+
+    for name, url in DATASETS.items():
+
+        print("Downloading", name)
+
+        r = requests.get(url)
+
+        path = f"real-data/{name}.csv"
+
+        with open(path, "wb") as f:
+            f.write(r.content)
+
+        print("Saved", path)
+
+
+if __name__ == "__main__":
+    download()
