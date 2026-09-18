6483f4b6043bd821ec89b9587d1ac9ebea73eb17
AhmedPeaRL <89841973+AhmedPeaRL@users.noreply.github.com>
2026-09-19 00:50:47 +0300

    Rename empirical_p_upper to diagnostic_empirical_p_upper

--- analysis/separation_test.py
+++ analysis/separation_test.py
@@ -371,7 +371,7 @@ def separation_score(
         "null_degenerate":
             null_degenerate,
         "empirical_p_upper":
-            empirical_p_upper,
+            diagnostic_empirical_p_upper,
         "null_exceedances":
             int(exceedances),
         "percentile_rank":
